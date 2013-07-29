3252c4221268d7230b99e419d10dd982bc7c9c3d
jmtast <jm@tast.com.ar>
2013-07-29 11:53:50 -0300

    active numbers, clear and dot buttons

--- WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/MainWindow.xaml.cs
@@ -20,6 +20,7 @@ namespace WpfApplication1
     
     public partial class MainWindow : Window
     {
+        bool hasDot = false;
 
         public MainWindow()
         {
@@ -29,6 +30,7 @@ namespace WpfApplication1
             operations.Items.Add(new Multiply());
             operations.Items.Add(new Divide());
             operations.SelectedItem = operations.Items[0];
+            writeMessage("0");
         }
 
         private void operation_Click(object sender, RoutedEventArgs e, Operations operation)
@@ -97,6 +99,88 @@ namespace WpfApplication1
         private void writeMessage(String message)
         {
             resultBlock.Text = message;
+        }
+
+        private void addCharacter(String message)
+        {
+            if (resultBlock.Text.Equals("0") && !(message.Equals(".")))
+            {
+                writeMessage(message);
+            }
+            else
+            {
+                resultBlock.Text += message;
+            }
+        }
+
+        private void doit_Click_1(object sender, RoutedEventArgs e)
+        {
+            operation_Click(this, null, (Operations)operations.SelectedItem);
+        }
+
+        private void Click0(object sender, RoutedEventArgs e)
+        {
+            addCharacter("0");
+        }
+
+        private void Click1(object sender, RoutedEventArgs e)
+        {
+            addCharacter("1");
+        }
+
+        private void Click2(object sender, RoutedEventArgs e)
+        {
+            addCharacter("2");
+        }
+
+        private void Click3(object sender, RoutedEventArgs e)
+        {
+            addCharacter("3");
+        }
+
+        private void Click4(object sender, RoutedEventArgs e)
+        {
+            addCharacter("4");
+        }
+
+        private void Click5(object sender, RoutedEventArgs e)
+        {
+            addCharacter("5");
+        }
+
+        private void Click6(object sender, RoutedEventArgs e)
+        {
+            addCharacter("6");
+        }
+
+        private void Click7(object sender, RoutedEventArgs e)
+        {
+            addCharacter("7");
+        }
+
+        private void Click8(object sender, RoutedEventArgs e)
+        {
+            addCharacter("8");
+        }
+
+        private void Click9(object sender, RoutedEventArgs e)
+        {
+            addCharacter("9");
+        }
+
+        private void ClickC(object sender, RoutedEventArgs e)
+        {
+            writeMessage("0");
+            hasDot = false;
+        }
+
+        private void ClickDot(object sender, RoutedEventArgs e)
+        {
+            if (!hasDot)
+            {
+                hasDot = true;
+                addCharacter(".");
+            }
         }
     }
 }
